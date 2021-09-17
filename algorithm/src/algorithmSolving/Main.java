bc97948a545a292b17bcab9ba1ddfcb806d22528
kzzzzmi <jaemin21685@gmail.com>
2021-09-17 14:07:04 +0900

    math algorithm

--- algorithm/src/algorithmSolving/Main.java
+++ algorithm/src/algorithmSolving/Main.java
@@ -4,42 +4,59 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
-import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int num = Integer.parseInt(br.readLine());
+		int test = Integer.parseInt(br.readLine());
 		
-		for(int i = 0; i < num; i++) {
-			String str = br.readLine();
-			if(isVPS(str)) {
-				System.out.println("YES");
-			} else {
-				System.out.println("NO");
-			}
-		}	
+		for(int i = 0; i < test; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			
+			System.out.println(teleportNum(y - x));
+		}
+		
 	}
 	
-	public static boolean isVPS(String str) {
-		Stack<Integer> stack = new Stack<Integer>();
-		char[] ch = str.toCharArray();
-		for(int i = 0; i < ch.length; i++) {
-			if(stack.isEmpty() && ch[i] == ')') {
-				return false;
-			} else {
-				if(ch[i] == '(') {
-					stack.push(0);
-				} else {
-					stack.pop();
-				}
-			}
+	public static int teleportNum(int len) {
+		if(len <= 3) {
+			return len;
 		}
-		if(stack.isEmpty()) {
-			return true;
+		
+		int temp = len;
+		
+		int i;
+		int cnt = 0;
+		int sum = 0;
+		for(i = 1; i <= Math.floor(Math.sqrt(len)); i++) {
+			sum += i;
+			temp -= i;
+			cnt++;
 		}
-		return false;
+		i--;
+		
+		while(temp >= sum) {
+			temp -= i;
+			cnt++;
+		}
+		i--;
+		
+		sum = 0;
+		while(i != 0) {
+			sum += i;
+			i--;
+			cnt++;
+		}
+		
+		if(sum != temp) {
+			cnt++;
+		}	
+		return cnt;
 	}
+
 }
