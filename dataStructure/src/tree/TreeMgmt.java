5f7950b34a0b08e69c3016c228fa28fa30e562cd
kzzzzmi <jaemin21685@gmail.com>
2021-09-23 23:38:19 +0900

    tree delete

--- dataStructure/src/tree/TreeMgmt.java
+++ dataStructure/src/tree/TreeMgmt.java
@@ -1,4 +1,5 @@
 package tree;
+
 class Node {
 	Node left;
 	Node right;
@@ -37,17 +38,17 @@ public class TreeMgmt {
 		}
 		return true;
 	}
-	
+
 	public Node searchNode(int data) {
-		if(this.head == null) {
+		if (this.head == null) {
 			return null;
-		} 
+		}
 		Node findNode = this.head;
-		while(findNode != null) {
-			if(findNode.data == data) {
+		while (findNode != null) {
+			if (findNode.data == data) {
 				return findNode;
 			}
-			if(data < findNode.data) {
+			if (data < findNode.data) {
 				findNode = findNode.left;
 			} else {
 				findNode = findNode.right;
@@ -55,6 +56,75 @@ public class TreeMgmt {
 		}
 		return null;
 
+	}
+
+	public boolean delete(int data) {
+		boolean searched = false;
+
+		Node currParentNode = this.head;
+		Node currNode = this.head;
+
+		// case1 : Node가 하나도 없을 때
+		if (this.head == null) {
+			return false;
+			
+			// case2 : Node 가 단지 하나만 있고, 해당 Node가 삭제할 Node일 때
+		} else if (this.head.data == data && this.head.left == null && this.head.right == null) {
+			this.head = null;
+			return true;
+		} else {
+			while (currNode != null) {
+				if (currNode.data == data) {
+					searched = true;
+					break;
+				} else if (data < currNode.data) {
+					currParentNode = currNode;
+					currNode = currNode.left;
+				} else {
+					currParentNode = currNode;
+					currNode = currNode.right;
+				}
+			}
+
+			if (searched == false) {
+				return false;
+			}
+		}
+
+		// 여기 구문까지 실행되면
+		// currNode는 해당 데이터를 가지고 있는 Node가 있고
+		// currParentNode는 그 Node의 부모 Node가 있음
+
+		// case1 : 삭제할 Node가 Leaf Node일 때
+		if(currNode.left == null && currNode.right == null) {
+			if(data < currParentNode.data) {
+				currParentNode.left = null;
+				currNode = null;
+			} else {
+				currParentNode.right = null;
+				currNode = null;
+			}
+			return true;
+			// 삭제할 Node가 child Node를 한 개 가지고 있을 떄
+		} else if(currNode.left != null && currNode.right == null) {
+			if(data < currParentNode.data) {
+				currParentNode.left = currNode.left;
+				currNode = null;
+			} else {
+				currParentNode.right = currNode.left;
+				currNode = null;
+			}
+			return true;
+		} else if(currNode.left == null && currNode.right != null) {
+			if(data < currParentNode.data) {
+				currParentNode.left = currNode.right;
+				currNode = null;
+			} else {
+				currParentNode.right = currNode.right;
+				currNode = null;
+			}
+			return true;
+		}
 	}
 
 }
