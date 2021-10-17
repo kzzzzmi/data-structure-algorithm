2f7c1d8126b72a57bc27985335db4cc7e8af5026
kzzzzmi <jaemin21685@gmail.com>
2021-10-17 21:05:16 +0900

    mergeSort solving

--- algorithm/src/algorithmSolving/Main.java
+++ algorithm/src/algorithmSolving/Main.java
@@ -1,51 +1,67 @@
 package algorithmSolving;
 
+import java.util.ArrayList;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.StringTokenizer;
 
 public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int row = Integer.parseInt(st.nextToken());
-		int col = Integer.parseInt(st.nextToken());
-		boolean[][] board = new boolean[row+1][col+1];
+		StringBuilder sb = new StringBuilder();
 		
-		for(int i = 1; i <= row; i++) {
-			String wb = br.readLine();
-			for(int j = 1; j <= col; j++) {
-				if(wb.charAt(j) == 'W') {
-					board[i][j] = true;
-				}
-			}
+		int size = Integer.parseInt(br.readLine());
+		ArrayList<Integer> array = new ArrayList<>();
+		
+		while(size > 0) {
+			array.add(Integer.parseInt(br.readLine()));
+			size--;
 		}
+		array = mergeSplitFunc(array);
 		
-		int rowT = row - 7;
-		int colT = col - 7;
-		int cnt = 0;
-		boolean index = false;
-		
-		while(rowT > 0) {
-			for(int i = rowT; i < rowT + 7; i++) {
-				for(int j = colT; j < colT + 7; j++) {
-					if(board[i][j] == board[i+1][j+1]) {
-						cnt++;
-					}
-				}
-			}
-			if(colT > 1) {
-				colT--;
-			} else if(colT == 1){
-				rowT--;
-				colT = col - 7;
-			}
+		for(int i = 0 ; i < array.size(); i++) {
+			sb.append(array.get(i)).append('\n');
 		}
-		
+		System.out.println(sb);
 		
 	}
 	
+	public static ArrayList<Integer> mergeSplitFunc(ArrayList<Integer> array) {
+		if(array.size() <= 1) {
+			return array;
+		}
+		int mid = array.size() / 2;
+		
+		ArrayList<Integer> leftArray = mergeSplitFunc(new ArrayList<>(array.subList(0, mid)));
+		ArrayList<Integer> rightArray = mergeSplitFunc(new ArrayList<>(array.subList(mid, array.size())));
+		
+		return mergeFunc(leftArray, rightArray);
+	}
+	
+	public static ArrayList<Integer> mergeFunc(ArrayList<Integer> leftArray, ArrayList<Integer> rightArray) {
+		int left = 0;
+		int right = 0;
+		ArrayList<Integer> mergeArray = new ArrayList<>();
+		
+		while(left < leftArray.size() && right < rightArray.size()) {
+			if(leftArray.get(left) < rightArray.get(right)) {
+				mergeArray.add(leftArray.get(left++));
+			} else {
+				mergeArray.add(rightArray.get(right++));
+			}
+		}
+		
+		while(left < leftArray.size()) {
+			mergeArray.add(leftArray.get(left++));
+		}
+		while(right < rightArray.size()) {
+			mergeArray.add(rightArray.get(right++));
+		}
+		
+		leftArray = null;
+		rightArray = null;
+		return mergeArray;
+	}
 }
