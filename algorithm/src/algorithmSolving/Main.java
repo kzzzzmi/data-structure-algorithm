b76ff580e996bf679af71a63532e62466386ce82
kzzzzmi <jaemin21685@gmail.com>
2021-10-20 22:20:32 +0900

    Map, Entry

--- algorithm/src/algorithmSolving/Main.java
+++ algorithm/src/algorithmSolving/Main.java
@@ -1,67 +1,35 @@
 package algorithmSolving;
 
-import java.util.ArrayList;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class Main {
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
+		HashMap<String, Integer> map = new HashMap<>();
+		map.put("¿Î¿Î1", 12314);
+		map.put("¿Î¿Î2", 12314);
+		map.put("¿Î¿Î3", 12315);
+		map.put("¿Î¿Î3", 12314);
+		map.put("¿Î¿Î4", 12315);
+		map.put("¿Î¿Î5", 12314);
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		int size = Integer.parseInt(br.readLine());
-		ArrayList<Integer> array = new ArrayList<>();
-		
-		while(size > 0) {
-			array.add(Integer.parseInt(br.readLine()));
-			size--;
-		}
-		array = mergeSplitFunc(array);
-		
-		for(int i = 0 ; i < array.size(); i++) {
-			sb.append(array.get(i)).append('\n');
-		}
-		System.out.println(sb);
-		
-	}
-	
-	public static ArrayList<Integer> mergeSplitFunc(ArrayList<Integer> array) {
-		if(array.size() <= 1) {
-			return array;
-		}
-		int mid = array.size() / 2;
-		
-		ArrayList<Integer> leftArray = mergeSplitFunc(new ArrayList<>(array.subList(0, mid)));
-		ArrayList<Integer> rightArray = mergeSplitFunc(new ArrayList<>(array.subList(mid, array.size())));
-		
-		return mergeFunc(leftArray, rightArray);
-	}
-	
-	public static ArrayList<Integer> mergeFunc(ArrayList<Integer> leftArray, ArrayList<Integer> rightArray) {
-		int left = 0;
-		int right = 0;
-		ArrayList<Integer> mergeArray = new ArrayList<>();
-		
-		while(left < leftArray.size() && right < rightArray.size()) {
-			if(leftArray.get(left) < rightArray.get(right)) {
-				mergeArray.add(leftArray.get(left++));
-			} else {
-				mergeArray.add(rightArray.get(right++));
+		Iterator<String> ir  = map.keySet().iterator();
+		while(ir.hasNext()) {
+			String st = ir.next();
+			if(st.equals("¿Î¿Î2")) {
+				System.out.println(st);
 			}
 		}
 		
-		while(left < leftArray.size()) {
-			mergeArray.add(leftArray.get(left++));
-		}
-		while(right < rightArray.size()) {
-			mergeArray.add(rightArray.get(right++));
+		for(Map.Entry<String, Integer> entry : map.entrySet()) {
+			if(entry.getValue() == 12315) {
+				System.out.println(entry.getKey());
+			}
 		}
 		
-		leftArray = null;
-		rightArray = null;
-		return mergeArray;
+		
 	}
+	
 }
