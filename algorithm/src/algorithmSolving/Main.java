c24601b596722f269ab177a07fb985c4d3777160
kzzzzmi <jaemin21685@gmail.com>
2021-10-25 20:46:33 +0900

    Update Main.java

--- algorithm/src/algorithmSolving/Main.java
+++ algorithm/src/algorithmSolving/Main.java
@@ -1,35 +1,85 @@
 package algorithmSolving;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.ArrayList;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.StringTokenizer;
 
 public class Main {
+	static int H;
+	static int W;
 	
-	public static void main(String[] args) {
-		HashMap<String, Integer> map = new HashMap<>();
-		map.put("¿Î¿Î1", 12314);
-		map.put("¿Î¿Î2", 12314);
-		map.put("¿Î¿Î3", 12315);
-		map.put("¿Î¿Î3", 12314);
-		map.put("¿Î¿Î4", 12315);
-		map.put("¿Î¿Î5", 12314);
+	public static void main(String[] args) throws IOException {
 		
-		Iterator<String> ir  = map.keySet().iterator();
-		while(ir.hasNext()) {
-			String st = ir.next();
-			if(st.equals("¿Î¿Î2")) {
-				System.out.println(st);
-			}
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		H = Integer.parseInt(st.nextToken());
+		W = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(br.readLine());
+		ArrayList<Nemo> nemo = new ArrayList<>();
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			nemo.add(new Nemo(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
 		}
+		Collections.sort(nemo);
 		
-		for(Map.Entry<String, Integer> entry : map.entrySet()) {
-			if(entry.getValue() == 12315) {
-				System.out.println(entry.getKey());
-			}
-		}
-		
+		int area = maxArea(nemo);		
+		System.out.println(area);
 		
 	}
 	
+	public static int maxArea(ArrayList<Nemo> nemo) {
+		for(int i = 0; i < nemo.size() - 1; i++) {
+			Nemo o = nemo.get(i);
+			int x = o.width;
+			int y = o.height;
+			if(checkRange(x, y)) {
+				continue;
+			}
+			for(int j = i + 1; j < nemo.size(); j++) {
+				Nemo n = nemo.get(j);
+				if(checkRange(n.width, n.height)) {
+					continue;
+				}
+				if(((n.width <= W && n.height <= H - y) || (n.width <= W - x && n.height <= H)) || 
+						((n.width <= W && n.height <= H - x) || (n.width <= W - y && n.height <= H))) {
+					return (o.width * o.height) + (n.width * n.height);
+				} else if(((n.height <= W && n.width <= H - y) || (n.height <= W - x && n.width <= H)) || 
+						((n.height <= W && n.width <= H - x) || (n.height <= W - y && n.width <= H))) {
+					return (o.width * o.height) + (n.width * n.height);
+				}
+			}
+		}
+		return 0;
+	}
+	
+	public static boolean checkRange(int x, int y) {
+		if((x >= W && y >= W) || (x >= H && y >= H)) {
+			return true;
+		}
+		return false;
+	}
+	
+}
+
+class Nemo implements Comparable {
+	int width;
+	int height;
+	
+	public Nemo(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+	
+	public int compareTo(Object ob) {
+		Nemo n = (Nemo)ob;
+		if(this.width * this.height > n.width * n.height) {
+			return -1;
+		} else {
+			return 1;
+		}
+	}
 }
