f0baa4deae74bcff6e91bf4bffbd2eacfbde1e6b
kzzzzmi <jaemin21685@gmail.com>
2022-07-14 21:13:11 +0900

    merge sort 구현 및 알고리즘 풀이

--- algorithm/src/algorithmSolving/Main.java
+++ algorithm/src/algorithmSolving/Main.java
@@ -3,93 +3,39 @@ package algorithmSolving;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.Set;
+import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int[] array;
-	static HashMap<Integer, Integer> modeArray = new HashMap<Integer, Integer>();
-	
+
 	public static void main(String[] args) throws IOException {
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int num = Integer.parseInt(br.readLine());
-		array = new int[num];
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[][] arr = new int[2][n];
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
 		
-		for(int i = 0; i < array.length; i++) {
-			int value = Integer.parseInt(br.readLine());;
-			modeArray.put(value, modeArray.containsKey(value) ? modeArray.get(value) + 1 : 1);
-			array[i] = value;
+		for(int i = 0; i < n; i++) {
+			arr[0][i] = Integer.parseInt(st.nextToken());
+			arr[1][i] = arr[0][i];
 		}
-		Arrays.sort(array);
 		
-		StringBuilder sb = new StringBuilder();
-		sb.append(average()).append('\n');
-		sb.append(middleValue()).append('\n');
-		sb.append(mode()).append('\n');
-		sb.append(range());
+		Arrays.sort(arr[0]);
 		
-		System.out.println(sb);
-		
-	}
-	
-	public static int range() {
-		return array[array.length - 1] - array[0];
-	}
-	
-	public static int mode() {
-		Set<Integer> keySet = modeArray.keySet();
-		ArrayList<Integer> maxKey = new ArrayList<>();
-		
-		int maxValue = 0;
-		
-		for(int i : keySet) {
-			if(modeArray.get(i) > maxValue) {
-				maxKey.clear();
-				maxKey.add(i);
-				maxValue = modeArray.get(i);
-			} else if(modeArray.get(i) == maxValue) {
-				maxKey.add(i);
+		int rank = 0;
+		for(int i : arr[0]) {
+			if(!map.containsKey(i)) {
+				map.put(i, rank++);
 			}
 		}
 		
-		if(maxKey.size() == 1) {
-			return maxKey.get(0);
-		} else {
-			maxKey.sort(new Comparator<Integer>() {
-
-				@Override
-				public int compare(Integer o1, Integer o2) {
-					return o1 - o2;
-				}
-			});
-			return maxKey.get(1);
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < n; i++) {
+			sb.append(map.get(arr[1][i])).append(" ");
 		}
-		
-	}
-	
-	public static int average() {
-		int sum = 0;
-		for(int i = 0; i < array.length; i++) {
-			sum += array[i];
-		}
-		
-		double avg = ((double) sum / array.length);
-		int result;
-		if(avg >= 0) {
-			result = (int)(avg + 0.5);
-		} else {
-			result = (int)(avg - 0.5);
-		}
-		return result;
-	}
-	
-	public static int middleValue() {
-		return array[array.length / 2];
+		System.out.println(sb);
 	}
 
 }
--- algorithm/src/sort/MySort.java
+++ algorithm/src/sort/MySort.java
@@ -4,64 +4,100 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class MySort {
-	
-	public ArrayList<Integer> bubbleSort(ArrayList<Integer> array) {		
-		for(int i = 0; i < array.size() - 1; i++) {
+
+	public ArrayList<Integer> bubbleSort(ArrayList<Integer> array) {
+		for (int i = 0; i < array.size() - 1; i++) {
 			boolean swap = false;
-			
-			for(int j = 0; j < array.size() - 1 - i; j++) {
-				if(array.get(j) > array.get(j + 1)) {
+
+			for (int j = 0; j < array.size() - 1 - i; j++) {
+				if (array.get(j) > array.get(j + 1)) {
 					Collections.swap(array, j, j + 1);
 					swap = true;
 				}
 			}
-			
-			if(!swap) {
+
+			if (!swap) {
 				break;
 			}
 		}
 		return array;
 	}
-	
+
 	public ArrayList<Integer> insertionSort(ArrayList<Integer> array) {
-		for(int i = 1; i < array.size(); i++) {
+		for (int i = 1; i < array.size(); i++) {
 			int index = i;
-			while(index >= 1 && array.get(index) < array.get(index - 1)) {
+			while (index >= 1 && array.get(index) < array.get(index - 1)) {
 				Collections.swap(array, index, index - 1);
 				index--;
 			}
 		}
 		return array;
 	}
-	
+
 	public ArrayList<Integer> selectionSort(ArrayList<Integer> array) {
-		for(int i = 0; i < array.size() - 1; i++) {
+		for (int i = 0; i < array.size() - 1; i++) {
 			int minIdx = i;
-			for(int j = i + 1; j < array.size(); j++) {
-				if(array.get(j) < array.get(minIdx)) {
+			for (int j = i + 1; j < array.size(); j++) {
+				if (array.get(j) < array.get(minIdx)) {
 					minIdx = j;
 				}
 			}
-			if(minIdx != i) {
+			if (minIdx != i) {
 				Collections.swap(array, i, minIdx);
 			}
 		}
 		return array;
 	}
 	
-	public static void main(String[] args) {
-		MySort sort = new MySort();
-		
-		ArrayList<Integer> array = new ArrayList<Integer>();
-		for(int i = 0; i < 100; i++) {
-			array.add((int)(Math.random() * 100 + 1));
+	public static ArrayList<Integer> splitArray(ArrayList<Integer> array) {
+		if(array.size() <= 1) {
+			return array;
 		}
+		int mid = array.size() / 2;
+		ArrayList<Integer> leftArr = splitArray(new ArrayList<Integer>(array.subList(0, mid)));
+		ArrayList<Integer> rightArr = splitArray(new ArrayList<Integer>(array.subList(mid, array.size())));
 		
-		sort.bubbleSort(array);
-		
-		for(int i : array) {
-			System.out.print(i);
-		}
+		return mergeArr(leftArr, rightArr);
 	}
 	
+	public static ArrayList<Integer> mergeArr(ArrayList<Integer> leftArr, ArrayList<Integer> rightArr) {
+		ArrayList<Integer> mergeArr = new ArrayList<Integer>();
+		int leftIdx = 0;
+		int rightIdx = 0;
+		
+		while(leftIdx < leftArr.size() && rightIdx < rightArr.size()) {
+			if(leftArr.get(leftIdx) < rightArr.get(rightIdx)) {
+				mergeArr.add(leftArr.get(leftIdx++));
+			} else {
+				mergeArr.add(rightArr.get(rightIdx++));
+			}
+		}
+		
+		if(leftIdx >= leftArr.size()) {
+			while(rightIdx < rightArr.size()) {
+				mergeArr.add(rightArr.get(rightIdx++));
+			}
+		}
+		if(rightIdx >= rightArr.size()) {
+			while(leftIdx < leftArr.size()) {
+				mergeArr.add(leftArr.get(leftIdx++));
+			}
+		}
+		
+		return mergeArr;
+	}
+
+	public static void main(String[] args) {
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		for(int i = 0; i < 100; i++) {
+			arr.add((int)(Math.random() * 100 + 1));
+		}
+		
+		arr = splitArray(arr);
+		
+		for(int i : arr) {
+			System.out.print(i + " ");
+		}
+	}
+
 }
