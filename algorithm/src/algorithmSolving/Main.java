cf7e4e2effbd69bb2b2fbd5c90a37aec67bbe783
kzzzzmi <jaemin21685@gmail.com>
2022-07-20 14:57:12 +0900

    no message

--- algorithm/src/algorithmSolving/Main.java
+++ algorithm/src/algorithmSolving/Main.java
@@ -1,3 +1,11 @@
+/*
+ * 규칙
+ *  - answer과 choice를 비교해서 strike와 ball을 받음
+ *  - 이때, 비교를 통해서 얻은 strike와 ball 값은 cases 내의 경우의 수들과 choice를 비교하여 얻는 compStrike와 compBall과 비교하게 된다
+ *  - cases의 요소들과 choice를 비교했서 얻은 compStrike와 compBall 값과 위의 strike와 ball이 일치하지 않으면 answer이 될 수 없는 조건을 찾음
+ *  - 위의 조건을 이용하여 완전 탐색을 해서 매 입력마다 cases의 요소를 지움
+ */
+
 package algorithmSolving;
 
 import java.io.BufferedReader;
@@ -5,50 +13,130 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-
 public class Main {
-	
+
 	static ArrayList<String> cases = new ArrayList<>();
 	static String answer;
-	
+	static int strike;
+	static int ball;
+
 	public static void main(String[] args) throws IOException {
 
-		boolean flag = true;
-		int gameCount = 0;
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		answer = br.readLine();
-		initCase();
-		
-		while(flag) {
-			
+		int allGameCount = 0;
+		int count = 0;
+
+		while (count < 1) {
+			while (true) {
+				System.out.print("1 ~ 9 까지 중복되지 않는 세자리 수를 입력해주세요 : ");
+				answer = br.readLine();
+				if (validation(Integer.parseInt(answer))) {
+					break;
+				}
+				System.out.println();
+			}
+//			answer = randomInput();
+			initCase();
+
+			while (true) {
+				int random = (int) (Math.random() * cases.size());
+				String choice = cases.remove(random);
+				System.out.println("Bot : " + choice);
+				getStrikeBall(choice);
+				System.out.println("Strike : " + strike + ", Ball : " + ball);
+				System.out.println();
+				allGameCount++;
+
+				if (strike == 3 || cases.size() <= 1) {
+					break;
+				}
+
+				for (int index = 0; index < cases.size(); index++) {
+					removeElements(choice, index);
+				}
+			}
+			count++;
+		}
+		System.out.println(count + "번 실행 결과 정답을 찾는데 걸린 횟수 평균 : " + allGameCount / count);
+	}
+
+	// strike, ball 그리고 compStrike, compBall 을 비교하여 같지 않는 요소 제거
+	public static void removeElements(String choice, int index) {
+		int compStrike = 0;
+		int compBall = 0;
+
+		String caseWord = cases.get(index);
+		for (int j = 0; j < 3; j++) {
+			char choiceAlphabet = choice.charAt(j);
+
+			if (choiceAlphabet == caseWord.charAt(j)) {
+				compStrike++;
+			} else {
+				for (int k = 0; k < 3; k++) {
+					if (choiceAlphabet == caseWord.charAt(k)) {
+						compBall++;
+					}
+				}
+			}
 		}
 
+		if (!(strike == compStrike && ball == compBall)) {
+			cases.remove(index);
+		}
 	}
-	
+
+	// 평균 몇번에 맞추는지 테스트 위해서 랜덤 값을 입력으로 넣는 함수
+	public static String randomInput() {
+		while (true) {
+			int temp = (int) (Math.random() * 987 + 1);
+			if (validation(temp)) {
+				return Integer.toString(temp);
+			}
+		}
+	}
+
+	// 프로그램이 랜덤으로 입력한 값과 answer을 비교해서 strike와 ball 값을 변경하는 함수
+	public static void getStrikeBall(String choice) {
+		strike = 0;
+		ball = 0;
+		for (int i = 0; i < 3; i++) {
+			if (answer.charAt(i) == choice.charAt(i)) {
+				strike++;
+			} else {
+				for (int j = 0; j < 3; j++) {
+					if (answer.charAt(i) == choice.charAt(j)) {
+						ball++;
+					}
+				}
+			}
+		}
+	}
+
+	// 맨 처음 가능한 모든 경우의 수를 cases 리스트에 add하는 함수
 	public static void initCase() {
-		int num = 123;		
-		while(num < 988) {
-			if(!overlapCheck(num)) {
+		int num = 123;
+		while (num < 988) {
+			if (validation(num)) {
 				cases.add(Integer.toString(num));
 			}
 			num++;
 		}
 	}
-	
-	public static boolean overlapCheck(int num) {
+
+	// answer 및 randomInput, initCase 함수를 사용할 때 유효성 검사하는 함수
+	public static boolean validation(int num) {
 		int[] temp = new int[3];
 		temp[0] = num / 100;
 		temp[1] = num / 10 % 10;
 		temp[2] = num % 10;
-		
-		if(temp[0] == temp[1] || temp[0] == temp[2] || temp[1] == temp[2]) {
-			return true;
+
+		if (temp[0] == temp[1] || temp[0] == temp[2] || temp[1] == temp[2]) {
+			return false;
 		}
-		if(temp[0] == 0 || temp[1] == 0 || temp[2] == 0) {
-			return true;
+		if (temp[0] == 0 || temp[1] == 0 || temp[2] == 0) {
+			return false;
 		}
-		return false;
+		return true;
 	}
 
 }
--- algorithm/src/algorithmSolving/Test.java
+++ algorithm/src/algorithmSolving/Test.java
@@ -1,15 +1,70 @@
 package algorithmSolving;
 
-import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Test {
+	public static void main(String[] args) throws Exception {
 
-	public static void main(String[] args) throws IOException{
+       Scanner scanner = new Scanner(System.in);
 
-		ArrayList<Integer> array = new ArrayList<Integer>();
-		array.add(null);
-		System.out.println(array.get(1));
-		
-	}
+       int a1, a2, a3;
+       int n1, n2, n3;
+       int gameCnt = 0;
+       int strikeCnt = 0;
+       int ballCnt = 0;
+
+       System.out.println("3개의 숫자를 입력하여 문제를 내주세요 (1~9)");
+
+       a1 = scanner.nextInt();
+       a2 = scanner.nextInt();
+       a3 = scanner.nextInt();
+
+       n1 = 3;
+       n2 = 5;
+       n3 = 7;
+
+       if(a1==n1){  //첫번째 자리 숫자가 같을 경우 strikeCnt++
+        strikeCnt++;
+       }
+       if(a2==n2){ //두번째 자리 숫자가 같을 경우 strikeCnt++
+        strikeCnt++;
+       }
+       if(a3==n3){  //세번째 자리 숫자가 같을 경우 strikeCnt++
+        strikeCnt++;
+       }
+       if(a1==n2 || a1==n3){ // a1이 n2와 같거나 a1이 n3가 같을 경우 ballCnt++
+        ballCnt++;
+       }
+       if(a2==n1 || a2==n3){ // a2와 n1이 같거나 a2와 n3가 같을 경우 ballCnt++
+        ballCnt++;
+       }
+       if(a3==n1 || a3==n2){ // a3와 n1이 같거나 a3와 n2가 같은 경우 ballCnt++
+        ballCnt++;
+       }
+
+       System.out.println(n1+ " " +n2+ " " +n3+ " " +strikeCnt+ "S " +ballCnt+ "B");
+
+       switch (strikeCnt) {
+        case 1:
+            //하나씩 자리 고정하고 바꾸기
+            break;
+        case 2:
+            //
+            break;
+        case 3:
+            System.out.println("정답입니다!");
+            break;
+       }
+
+       switch (ballCnt) {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+       }
+
+    }
+
 }
