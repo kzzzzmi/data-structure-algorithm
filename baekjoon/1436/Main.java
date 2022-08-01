2d90a0a2bde3d3d977ade76a9e5c667122e35b31
kzzzzmi <85213949+kzzzzmi@users.noreply.github.com>
2022-08-01 21:10:32 +0900

    Create Main.java

---
+++ baekjoon/1436/Main.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		int num = 666;
+		int count = 1;
+		while(count != N) {
+			num++;
+			int currNum = num;
+			while(currNum >= 666) {
+				if(currNum % 1000 == 666) {
+					count++;
+					break;
+				}
+				currNum /= 10;				
+			}			
+		}
+		System.out.println(num);
+	}
+}
