50a14c2b35815df3fdaa9271722b5ee0122808d0
kzzzzmi <jaemin21685@gmail.com>
2022-08-01 21:13:58 +0900

    백준 문제 풀이

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -1,110 +1,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 
-class Edge implements Comparable<Edge> {
-	
-	int vertex;
-	int distance;
-	
-	public Edge(int vertex, int distance) {
-		this.vertex = vertex;
-		this.distance = distance;
-	}
-	
-	@Override
-	public String toString() {
-		return "vertex : " + vertex + ", distance :  " + distance;
-	}
-
-	@Override
-	public int compareTo(Edge o) {
-		return this.distance - o.distance;
-	}
-	
-}
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int E = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(br.readLine());
+		int num = 1;
+		int int_value = 666;
+		String str_value = "666";
 		
-		HashMap<Integer, ArrayList<Edge>> graph = new HashMap<Integer, ArrayList<Edge>>();
-		for(int i = 1; i <= N; i++) {
-			graph.put(i, new ArrayList<>());
-		}
-		
-		for(int i = 0; i < E; i++) {
-			st = new StringTokenizer(br.readLine());
-			int ver1 = Integer.parseInt(st.nextToken());
-			int ver2 = Integer.parseInt(st.nextToken());
-			int weight = Integer.parseInt(st.nextToken());
-			graph.get(ver1).add(new Edge(ver2, weight)); 
-			graph.get(ver2).add(new Edge(ver1, weight)); 
-		}
-		
-		st = new StringTokenizer(br.readLine());
-		int v1 = Integer.parseInt(st.nextToken());
-		int v2 = Integer.parseInt(st.nextToken());
-		
-		int way1 = 0;
-		way1 += dijkstra(graph, 1, v1);
-		way1 += dijkstra(graph, v1, v2);
-		way1 += dijkstra(graph, v2, N);
-		
-		int way2 = 0;
-		way2 += dijkstra(graph, 1, v2);
-		way2 += dijkstra(graph, v2, v1);
-		way2 += dijkstra(graph, v1, N);
-		
-		if(way1 >= 200000000 && way2 >= 200000000) {
-			System.out.println(-1);
-		} else {
-			System.out.println(way1 < way2 ? way1 : way2);
-		}	
-	}
-	
-	public static int dijkstra(HashMap<Integer, ArrayList<Edge>> graph, int start, int end) {
-		HashMap<Integer, Integer> distances = new HashMap<Integer, Integer>();
-		PriorityQueue<Edge> priorityQueue = new PriorityQueue<Edge>();
-		
-		for(int i = 1; i <= graph.size(); i++) {
-			distances.put(i, 200000000);
-		}
-		distances.put(start, 0);
-		priorityQueue.add(new Edge(start, 0));
-		
-		while(priorityQueue.size() > 0) {
-			Edge edge = priorityQueue.poll();
-			int vertex = edge.vertex;
-			int distance = edge.distance;
+		while(num != N) {
+			int_value += 1000;
+			str_value = Integer.toString(int_value);
 			
-			if(distances.get(vertex) < distance) {
-				continue;
-			}
-			
-			ArrayList<Edge> adjacents = graph.get(vertex);
-			for(int i = 0; i < adjacents.size(); i++) {
-				Edge adjacent = adjacents.get(i);
-				int adjacentVertex = adjacent.vertex;
-				int weight = adjacent.distance;
-				int currDistance = distance + weight;
-				
-				if(currDistance < distances.get(adjacentVertex)) {
-					distances.put(adjacentVertex, currDistance);
-					priorityQueue.add(new Edge(adjacentVertex, currDistance));
+			int index = str_value.indexOf("666") + 2;
+			if(index == str_value.length() - 1) {
+				num++;
+			} else {
+				int str_length = str_value.length();
+				str_value = str_value.substring(0, index + 1);
+				for(int i = index + 1; i < str_length; i++) {
+					str_value += "0";
 				}
+				int_value = Integer.parseInt(str_value);
+				num++;
+				while(str_value.indexOf("666") + 2 == index) {
+					if(num >= N) {
+						System.out.println(int_value);
+						return;
+					}
+					int_value++;
+					str_value = Integer.toString(int_value);
+					num++;
+				}
+				str_value = str_value.substring(0, str_value.length() - 3);
+				str_value += "666";
+				int_value = Integer.parseInt(str_value);
+				num--;
 			}
 		}
-		return distances.get(end);
+		System.out.println(int_value);
 	}
-
 }
--- algorithm/src/solve/Test.java
+++ algorithm/src/solve/Test.java
@@ -1,70 +1,30 @@
 package solve;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 
 public class Test {
-	public static void main(String[] args) throws Exception {
 
-       Scanner scanner = new Scanner(System.in);
-
-       int a1, a2, a3;
-       int n1, n2, n3;
-       int gameCnt = 0;
-       int strikeCnt = 0;
-       int ballCnt = 0;
-
-       System.out.println("3개의 숫자를 입력하여 문제를 내주세요 (1~9)");
-
-       a1 = scanner.nextInt();
-       a2 = scanner.nextInt();
-       a3 = scanner.nextInt();
-
-       n1 = 3;
-       n2 = 5;
-       n3 = 7;
-
-       if(a1==n1){  //첫번째 자리 숫자가 같을 경우 strikeCnt++
-        strikeCnt++;
-       }
-       if(a2==n2){ //두번째 자리 숫자가 같을 경우 strikeCnt++
-        strikeCnt++;
-       }
-       if(a3==n3){  //세번째 자리 숫자가 같을 경우 strikeCnt++
-        strikeCnt++;
-       }
-       if(a1==n2 || a1==n3){ // a1이 n2와 같거나 a1이 n3가 같을 경우 ballCnt++
-        ballCnt++;
-       }
-       if(a2==n1 || a2==n3){ // a2와 n1이 같거나 a2와 n3가 같을 경우 ballCnt++
-        ballCnt++;
-       }
-       if(a3==n1 || a3==n2){ // a3와 n1이 같거나 a3와 n2가 같은 경우 ballCnt++
-        ballCnt++;
-       }
-
-       System.out.println(n1+ " " +n2+ " " +n3+ " " +strikeCnt+ "S " +ballCnt+ "B");
-
-       switch (strikeCnt) {
-        case 1:
-            //하나씩 자리 고정하고 바꾸기
-            break;
-        case 2:
-            //
-            break;
-        case 3:
-            System.out.println("정답입니다!");
-            break;
-       }
-
-       switch (ballCnt) {
-        case 1:
-            break;
-        case 2:
-            break;
-        case 3:
-            break;
-       }
-
-    }
-
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		int num = 666;
+		int count = 1;
+		while(count != N) {
+			num++;
+			int currNum = num;
+			while(currNum >= 666) {
+				if(currNum % 1000 == 666) {
+					count++;
+					break;
+				}
+				currNum /= 10;				
+			}			
+		}
+		System.out.println(num);
+	}
 }
