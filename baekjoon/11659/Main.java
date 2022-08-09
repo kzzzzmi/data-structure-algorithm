bf4a8945d47ca34868d5be6ef078b93b9c893df0
kzzzzmi <85213949+kzzzzmi@users.noreply.github.com>
2022-08-09 19:12:32 +0900

    11659번: 구간 합 구하기 4

---
+++ baekjoon/11659/Main.java
@@ -0,0 +1,46 @@
+// data : 2202-08-09
+// link : https://www.acmicpc.net/source/47408588
+/*
+  배열을 활용해서 누적합을 시간 내에 구함
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] arr, sumArr;
+		int start, end, sum, temp;
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		sumArr = new int[N + 1];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			sumArr[i + 1] = sumArr[i] + arr[i];
+		}
+		
+		for(int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			start = Integer.parseInt(st.nextToken());
+			end = Integer.parseInt(st.nextToken());
+			
+			sum = sumArr[end];
+			temp = sumArr[start - 1];
+			
+			sb.append(sum - temp).append('\n');
+		}
+		
+		System.out.println(sb);
+	}
+}
