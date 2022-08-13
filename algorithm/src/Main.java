0c9e455c9bc6c2db02d363c83f0400ff94c643e8
kzzzzmi <jaemin21685@gmail.com>
2022-08-13 19:41:04 +0900

    백준 문제해결

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -1,72 +1,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.PriorityQueue;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
-	
-	static HashSet<Integer> checkNum = new HashSet<Integer>();
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
 		int N = Integer.parseInt(br.readLine());
-						
-		PriorityQueue<Integer> measures = new PriorityQueue<Integer>();
-		int[] arr = new int[N];
-		int minNum = Integer.MAX_VALUE;
-		int value;
-		
-		for(int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-			if(arr[i] < minNum) {
-				minNum = arr[i];
-			}
+		st = new StringTokenizer(br.readLine());
+
+		Stack<Integer> stack = new Stack<Integer>();
+		int[] inputs = new int[N];
+		int[] count = new int[1000001];
+		int[] answer = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			inputs[i] = Integer.parseInt(st.nextToken());
+			count[inputs[i]]++;
 		}
-		
-		measures = getMeasure(minNum);
-		measures.remove(1);
-		
-		while(!measures.isEmpty()) {
-			value = measures.poll();
-			checkNum.add(value);
-			
-			boolean equalCheck = true;
-			int standardRes = arr[0] % value;
-			for(int i = 1; i < arr.length; i++) {
-				int temp = arr[i] % value;
-				if(temp != standardRes) {
-					equalCheck = false;
-				}
-				if(!measures.contains(temp) && !checkNum.contains(temp) && temp != 0) {
-					measures.add(temp);
-				}
+
+		stack.push(0);
+
+		for (int i = 1; i < N; i++) {
+			while (!stack.empty() && count[inputs[stack.peek()]] < count[inputs[i]]) {
+				answer[stack.pop()] = inputs[i];
 			}
-			
-			if(equalCheck) {
-				sb.append(value).append(' ');
-			}
+
+			stack.push(i);
 		}
+
+		while (!stack.empty()) {
+			answer[stack.pop()] = -1;
+		}
+
+		for (int i = 0; i < N; i++) {
+			sb.append(answer[i]).append(' ');
+		}
+
 		System.out.println(sb);
-		
-	}
-	
-	public static PriorityQueue<Integer> getMeasure(int minNum) {
-		PriorityQueue<Integer> measures = new PriorityQueue<Integer>();
-		int sqrt = (int)Math.sqrt(minNum);
-		
-		for(int i = 1; i <= sqrt; i++) {
-			if(minNum % i == 0) {
-				measures.add(i);
-				measures.add(minNum / i);
-				checkNum.add(i);
-				checkNum.add(minNum / i);
-			}
-		}
-		return measures;
-		
 	}
 
 }
