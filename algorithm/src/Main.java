15b885c8c810b1067690d988e5fa326dc5884fdc
kzzzzmi <jaemin21685@gmail.com>
2022-09-10 21:18:47 +0900

    no message

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -1,48 +1,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
-import java.util.StringTokenizer;
 
-public class Main {
+class Main {
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
 		int N = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine());
-
-		Stack<Integer> stack = new Stack<Integer>();
-		int[] inputs = new int[N];
-		int[] count = new int[1000001];
-		int[] answer = new int[N];
-
-		for (int i = 0; i < N; i++) {
-			inputs[i] = Integer.parseInt(st.nextToken());
-			count[inputs[i]]++;
-		}
-
-		stack.push(0);
-
-		for (int i = 1; i < N; i++) {
-			while (!stack.empty() && count[inputs[stack.peek()]] < count[inputs[i]]) {
-				answer[stack.pop()] = inputs[i];
+		
+		int[] A = new int[N];
+		int min = 1000001;
+		int count = 1;
+		int index = 0;
+		
+		for(int i = 0; i < N; i++) {
+			A[i] = Integer.parseInt(br.readLine());
+			if(A[i] < min) {
+				min = A[i];
+				index = i;
 			}
-
-			stack.push(i);
 		}
-
-		while (!stack.empty()) {
-			answer[stack.pop()] = -1;
+		
+		count += index;
+		
+		for(int i = index + 1; i < N; i++) {
+			if(A[i] > A[i + 1]) {
+				count++;
+			}
 		}
-
-		for (int i = 0; i < N; i++) {
-			sb.append(answer[i]).append(' ');
-		}
-
-		System.out.println(sb);
+		
+		System.out.println(count);
 	}
 
 }
